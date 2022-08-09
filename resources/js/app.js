eb08f05939b52906716c0ecacd9af131a2fa45b6
ckue-opportunity <ckue.opportunity@gmail.com>
2022-08-09 17:17:40 +0200

    installed Bootstrap, SASS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
-import './bootstrap';
-
+import '../sass/app.scss'; // import custom scss
+import './bootstrap'; // import bootstrap
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
-
 Alpine.start();
+
+// Test alert
+alert('This test alert pops up when the page is (re)loaded :-)');
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,8 +19,19 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
+
+        <!-- Scripts -->
+        @vite(['resources/js/app.js'])
+
     </head>
     <body class="antialiased">
+        <div class="brown">
+            <h1>Hello I am a text.</h1>
+        </div>
+        <div class="alert alert-primary" role="alert">
+            Bootstrap Proof: A simple primary alert—check it out!
+        </div>
+
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
