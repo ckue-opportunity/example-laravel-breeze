80ea8f9dae82386a47564323cedf4a0ac0c6eeb0
ckue-opportunity <ckue.opportunity@gmail.com>
2022-08-22 12:12:04 +0200

    update after npm run build

--- run-build-commit.sh
+++ run-build-commit.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 # PRECONDITION: RUN THIS SCRIPT IN LARAVEL DOCUMENT ROOT.
-echo "Build vite manifest and push data to GitHub.";
+echo "Create public build and commit all changes to Git.";
 
 # Delete existing build_ folder, generate vite manifest and copy the generated build into build_
 rm -R public/build_
 npm run build
 cp -R public/build public/build_
+
+# Git: Add and commit all changes
+git add -A
+git commit -m "update after npm run build"
+
